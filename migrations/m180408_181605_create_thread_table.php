694e4bd03543716b1af05febc638f130fd4760eb
Uthpala Heenatigala <uiheenatigala@gmail.com>
2018-04-09 01:04:01 +0530

    adding the migrations

---
+++ migrations/m180408_181605_create_thread_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `thread`.
+ */
+class m180408_181605_create_thread_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('thread', [
+            'id' => $this->primaryKey(),
+            'user_id' => $this->integer()->notNull(),
+            'title' => $this->string()->notNull(), 
+            'body' => $this->text(), 
+            'create_at' => $this->timestamp()->defaultValue(null),
+            'update_at' => $this->timestamp()->defaultValue(null)
+        ]);
+
+        // create index for user_id column
+        $this->createIndex(
+            'idx-thread-user_id',
+            'thread',
+            'user_id'
+        );
+
+        // create foreign ke 
+        $this->addForeignKey(
+            'fk-thread-user_id',
+            'thread', 
+            'user_id', 
+            'user', 
+            'id',
+            'CASCADE'
+        );
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey(
+            'fk-thread-user_id',
+            'thread'
+        );
+
+        $this->dropIndex(
+            'idx-thread-user_id',
+            'thread'
+        );
+        $this->dropTable('thread');
+    }
+}
---
+++ migrations/m180408_181613_create_reply_table.php
@@ -0,0 +1,85 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `reply`.
+ */
+class m180408_181613_create_reply_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('reply', [
+            'id' => $this->primaryKey(),
+            'thread_id' => $this->integer()->notNull(), 
+            'user_id' => $this->integer()->notNull(),
+            'body' => $this->text(),
+            'create_at' => $this->timestamp()->defaultValue(null),
+            'update_at' => $this->timestamp()->defaultValue(null)
+        ]);
+
+        // create index for user_id column
+        $this->createIndex(
+            'idx-reply-user_id',
+            'reply',
+            'user_id'
+        );
+
+        // create foreign ke 
+        $this->addForeignKey(
+            'fk-reply-user_id',
+            'reply', 
+            'user_id', 
+            'user', 
+            'id',
+            'CASCADE'
+        );
+
+        // create index for user_id column
+        $this->createIndex(
+            'idx-reply-thread_id',
+            'reply',
+            'thread_id'
+        );
+
+        // create foreign key 
+        $this->addForeignKey(
+            'fk-reply-thread_id',
+            'reply', 
+            'user_id', 
+            'user', 
+            'id',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey(
+            'fk-reply-user_id',
+            'reply'
+        );
+
+        $this->dropIndex(
+            'idx-reply-user_id',
+            'reply'
+        );
+
+        $this->dropForeignKey(
+            'fk-reply-thread_id',
+            'reply'
+        );
+
+        $this->dropIndex(
+            'idx-reply-thread_id',
+            'reply'
+        );
+        $this->dropTable('reply');
+    }
+}
